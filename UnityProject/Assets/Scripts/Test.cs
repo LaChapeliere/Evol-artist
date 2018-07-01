86ff90f60a40913891e36bbc0f5343a60dda6e1c
Max75013 <40629450+Max75013@users.noreply.github.com>
2018-07-01 12:13:34 +0200

    Variables dans l'inspecteur

--- UnityProject/Assets/Scripts/Test.cs
+++ UnityProject/Assets/Scripts/Test.cs
@@ -9,6 +9,15 @@ namespace Application
     public class Test : MonoBehaviour
     {
         Thread m_thread;
+        public int size = 6;
+        public int nbCreature = 100;
+        public int nbGeneration = 10;
+        //public List<string> DictionaryName = new List<string>();
+        //public List<List<int>> DictionaryValue = new List<List<int>>();
+        public Dictionary<string, Tuple<int, int>> globalEnv;
+        public string[] DictionaryName = new string[0];
+        public int[] DictionaryValue1 = new int[0];
+        public int[] DictionaryValue2 = new int[0];
 
         // Use this for initialization
         void Start()
@@ -20,16 +29,27 @@ namespace Application
        void TestFunction()
         {
             //Debug.Log("Pre-init");
-            Dictionary<string, Tuple<int, int>> globalEnv = new Dictionary<string, Tuple<int, int>>();
-            globalEnv.Add("test", new Tuple<int, int>(50, 10));
-            globalEnv.Add("test2", new Tuple<int, int>(45, 0));
+            globalEnv = new Dictionary<string, Tuple<int, int>>();
+            //globalEnv.Add("test", new Tuple<int, int>(50, 10));
+            //globalEnv.Add("test2", new Tuple<int, int>(45, 0));
+            if (DictionaryValue1.Length >= DictionaryName.Length & DictionaryValue2.Length >= DictionaryName.Length)
+            {
+                for (int i = 0; i < DictionaryName.Length; i++)
+                {
+                    globalEnv.Add(DictionaryName[i], new Tuple<int, int>(DictionaryValue1[i], DictionaryValue2[i]));
+                }
+            }
+            else
+            {
+                Debug.Log("Error, environement can't be load");
+            }
 
             string testGenome = "ABCEBDDEBCDBBDCBCADDCEBCDBBCDDDCEBCDBBCDADDDEBCDBBDBBCBCCDDCABCADCCEBDEBBDABDCBEBDDEEDABCCECADDDEBCADBCE";
 
             //Debug.Log("Pre-world");
-            World myWorld = new World(6, 100, testGenome, globalEnv);
+            World myWorld = new World(size, nbCreature, testGenome, globalEnv);
             //Debug.Log("Post-world");
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < nbGeneration; i++)
             {
                 //Debug.Log("Pre-simulation step");
                 myWorld.RunSimulationStep();
